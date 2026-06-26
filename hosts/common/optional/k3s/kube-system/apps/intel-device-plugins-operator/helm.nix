bd5395af1c5b59e79c29aad45298480e2401e3de
websheriff <websheriff@fastmail.com>
2026-06-25 20:36:12 -0500

    fix(kubernetes): add hashes for helm charts of intel-device-plugins-gpu/operator and nfd

--- hosts/common/optional/k3s/kube-system/apps/intel-device-plugins-operator/helm.nix
+++ hosts/common/optional/k3s/kube-system/apps/intel-device-plugins-operator/helm.nix
@@ -2,6 +2,7 @@
   services.k3s.autoDeployCharts.intel-device-plugins-operator = {
     name = "intel-device-plugins-operator";
     repo = "https://intel.github.io/helm-charts";
+    hash = "sha256-guPqpOlyIGUOvXvS/1bd5JB4ZGiU6Cmzj9Y7iqfigx8=";
     targetNamespace = "kube-system";
     createNamespace = false;
     version = "0.36.0";
--- hosts/common/optional/k3s/kube-system/apps/nfd/helm.nix
+++ hosts/common/optional/k3s/kube-system/apps/nfd/helm.nix
@@ -2,6 +2,7 @@
   services.k3s.autoDeployCharts.nfd = {
     name = "node-feature-discovery";
     repo = "https://kubernetes-sigs.github.io/node-feature-discovery/charts";
+    hash = "sha256-DLWb1wRE09Qm/9U/0kKuQzEY1+u50q0y09uZvopSPhY=";
     targetNamespace = "kube-system";
     createNamespace = false;
     version = "0.18.3";
