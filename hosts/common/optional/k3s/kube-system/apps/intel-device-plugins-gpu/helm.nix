84442fda2d5b9d799793887e252797cb883e3389
websheriff <websheriff@fastmail.com>
2026-06-25 20:19:54 -0500

    feat(kubernetes): add intel-device-plugins-gpu hash

--- hosts/common/optional/k3s/kube-system/apps/intel-device-plugins-gpu/helm.nix
+++ hosts/common/optional/k3s/kube-system/apps/intel-device-plugins-gpu/helm.nix
@@ -2,6 +2,7 @@
   services.k3s.autoDeployCharts.intel-device-plugins-gpu = {
     name = "intel-device-plugins-gpu";
     repo = "https://intel.github.io/helm-charts/";
+    hash = "sha256-xh4QWuIeXWzq1gXG0zfw6uTpJFKmSnS5juj2s3M3YWE="
     targetNamespace = "kube-system";
     createNamespace = false;
     version = "0.36.0";
