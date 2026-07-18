1570021136c5f951c6a5356bfd308cd978590f2b
websheriff <websheriff@fastmail.com>
2026-07-18 18:54:21 -0500

    fix(netbird-k3s): swap helm to sops template for placeholder entry for domain

--- hosts/common/optional/k3s/netbird/apps/netbird/helm.nix
+++ hosts/common/optional/k3s/netbird/apps/netbird/helm.nix
@@ -1,16 +1,28 @@
-{ ... }: {
-  services.k3s.autoDeployCharts.netbird-operator = {
-    name = "netbird-operator";
-    repo = "oci://ghcr.io/netbirdio/helm-charts/netbird-operator";
-    hash = "sha256-YbxJ6Nd1mA9SomYrTFZV8Xf/PovnGJaUzmpXCUg1mdE=";
-    targetNamespace = "netbird";
-    createNamespace = false;
-    version = "0.8.0";
-    values = {
-      netbirdAPI.keyFromSecret = {
-        name = "netbird-secret";
-        key = "access-token";
-      };
-    };
+{ config, ... }: {
+  sops.templates."netbird/netbird-operator-helm" = {
+    content = ''
+      apiVersion: helm.cattle.io/v1
+      kind: HelmChart
+      metadata:
+        name: netbird-operator
+        namespace: kube-system
+      spec:
+        chart: oci://ghcr.io/netbirdio/helm-charts/netbird-operator
+        version: "0.8.0"
+        targetNamespace: netbird
+        createNamespace: false
+        valuesContent: |
+          managementURL: "${config.sops.placeholder."netbird/domain}"
+          
+          netbirdAPI:
+            keyFromSecret:
+              name: "netbird-secret"
+              key: "access-token"
+    '';
+  
+    path = "/var/lib/rancher/k3s/server/manifests/netbird-operator-helm";
+    owner = "root";
+    group = "root";
+    mode = "0644";
   };
 }
--- hosts/common/optional/k3s/netbird/apps/netbird/network-router.nix
+++ hosts/common/optional/k3s/netbird/apps/netbird/network-router.nix
@@ -1,7 +1,7 @@
 { config, ... }: {
   sops.templates."netbird/netbird-network-router.yaml" = {
     content = ''
-      apiVersion: proxy.netbird.io/v1alpha1
+      apiVersion: netbird.io/v1alpha1
       kind: NetworkRouter
       metadata:
         name: prod-route
