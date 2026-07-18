8e8734887a26f76f24ec9f22f151371f6d415c18
websheriff <websheriff@fastmail.com>
2026-07-18 16:12:20 -0500

    fix(netbird-k3s): add hash

--- hosts/common/optional/k3s/netbird/apps/netbird/helm.nix
+++ hosts/common/optional/k3s/netbird/apps/netbird/helm.nix
@@ -2,7 +2,7 @@
   services.k3s.autoDeployCharts = {
     name = "netbird-operator";
     repo = "oci://ghcr.io/netbirdio/helm-charts/netbird-operator";
-    hash = "";
+    hash = "sha256-9mUW6gNwoN2SWc/l0fW4svPNOulXLl8ijqKyeSOGgJE%3D";
     targetNamespace = "netbird";
     createNamespace = false;
     version = "v0.3.1";
